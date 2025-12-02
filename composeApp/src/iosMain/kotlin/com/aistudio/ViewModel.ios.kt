f486f9f91683fa321418e8e91e1a14fcad0f2c93
ArturMazay <artmazay1986@gmail.com>
2025-12-02 10:29:47 -0800

    fix: исправлена реализация ViewModel для iOS и JVM - добавлен Dispatchers.Main

--- composeApp/src/iosMain/kotlin/com/aistudio/ViewModel.ios.kt
+++ composeApp/src/iosMain/kotlin/com/aistudio/ViewModel.ios.kt
@@ -2,11 +2,13 @@ package com.aistudio
 
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
 import kotlin.coroutines.CoroutineContext
 
 actual abstract class ViewModel {
     private val job = SupervisorJob()
-    private val scope = CoroutineScope(job)
+    private val scope = CoroutineScope(Dispatchers.Main + job)
     
     actual val viewModelScope: CoroutineScope
         get() = scope
--- composeApp/src/jvmMain/kotlin/com/aistudio/ViewModel.jvm.kt
+++ composeApp/src/jvmMain/kotlin/com/aistudio/ViewModel.jvm.kt
@@ -2,11 +2,12 @@ package com.aistudio
 
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.Dispatchers
 import kotlin.coroutines.CoroutineContext
 
 actual abstract class ViewModel {
     private val job = SupervisorJob()
-    private val scope = CoroutineScope(job)
+    private val scope = CoroutineScope(Dispatchers.Main + job)
     
     actual val viewModelScope: CoroutineScope
         get() = scope
